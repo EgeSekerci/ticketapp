30d923d09e11faf47cdd58f1444558b9b4b2fa5f
Ege Sekerci <yegesekerci@outlook.com>
2024-08-06 17:07:51 +0300

    Create and test first database connection
    - Add go.sum file for better dependency tracking
    
    - Update go.mod file for new dependencies
    
    - Add pq dependency for connecting to PostgreSQL database
    
    - Add godotenv dependency for securing credentials

--- main.go
+++ main.go
@@ -1,16 +1,64 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type dbConfig struct {
+	dbHost     string
+	dbPort     string
+	dbUser     string
+	dbPassword string
+	dbName     string
+}
+
+func loadDbConfig() dbConfig {
+	return dbConfig{
+		dbHost:     os.Getenv("DB_HOST"),
+		dbPort:     os.Getenv("DB_PORT"),
+		dbUser:     os.Getenv("DB_USER"),
+		dbPassword: os.Getenv("DB_PASSWORD"),
+		dbName:     os.Getenv("DB_NAME"),
+	}
+}
+
+func Connect() *sql.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env")
+	}
+
+	configs := loadDbConfig()
+
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		configs.dbHost, configs.dbPort, configs.dbUser, configs.dbPassword, configs.dbName)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Println("Error connecting to the database")
+	}
+
+	return db
+}
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hello world!")
+		db := Connect()
+		if db.Ping() != nil {
+			fmt.Println("Error connecting database")
+		} else {
+			fmt.Println("Connection to the database is successful")
+		}
 	})
 
 	port := ":8080"
