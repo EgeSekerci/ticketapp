165e6d927c953b524e23f2023040bc31ab325c1e
Ege Sekerci <yegesekerci@outlook.com>
2024-08-20 15:39:36 +0300

    Implement init function to initialize template parsing

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"net/http"
 
 	"ticketapp/routes"
 	"ticketapp/shared"
+	"ticketapp/tasks"
 )
+
+//go:embed templates/**
+var content embed.FS
+
+func init() {
+	var err error
+	err = tasks.ParseAllFiles(content)
+
+	shared.Check(err, "Error parsing templates")
+}
 
 func main() {
 	mux := http.NewServeMux()
