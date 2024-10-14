b0187f5d2b1c2b8f6328f5c5429c82c572faf434
Ege Sekerci <yegesekerci@outlook.com>
2024-10-14 15:45:26 +0300

    Implement JWT creation, validation, and route protection middleware

---
+++ tasks/jwt.go
@@ -0,0 +1,85 @@
+package tasks
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
+
+	"ticketapp/shared"
+)
+
+type User struct {
+	Id       int
+	Email    string
+	Password string
+	Role     string
+	Name     string
+}
+
+type contextKey string
+
+const (
+	claimsContextKey contextKey = "claims"
+)
+
+func createJWT(user *User) (string, error) {
+	err := godotenv.Load()
+	shared.Check(err, "Error loading .env")
+
+	claims := &jwt.MapClaims{
+		"exp":      time.Now().Add(time.Minute * 15).Unix(),
+		"userId":   user.Id,
+		"userRole": user.Role,
+	}
+
+	secret := os.Getenv("JWT_KEY")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
+
+func WithJWTAuth(handlerFunc http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("Authorization")
+		if err != nil || cookie.Value == "" {
+			http.Redirect(w, r, "/login", http.StatusFound)
+			return
+		}
+
+		claims, err := validateJWT(cookie.Value)
+
+		if err != nil || cookie.Value == "" {
+			http.Redirect(w, r, "/login", http.StatusFound)
+		}
+		ctx := context.WithValue(r.Context(), claimsContextKey, claims)
+		handlerFunc(w, r.WithContext(ctx))
+	}
+}
+
+func validateJWT(tokenString string) (jwt.MapClaims, error) {
+	err := godotenv.Load()
+	shared.Check(err, "Error loading .env")
+
+	secret := os.Getenv("JWT_KEY")
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+	if err != nil {
+		return nil, errors.New("Expired token")
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	} else {
+		return nil, errors.New("Invalid token")
+	}
+}
